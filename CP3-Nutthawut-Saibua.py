60c0e137659bf927e983f4774260d7c3a3252a4d
NutthawutS <nutthawut.saibua@gmail.com>
2021-08-29 17:51:55 +0700

    CP3-Nutthawut-Saibua
    exercise 4

---
+++ CP3-Nutthawut-Saibua.py
@@ -0,0 +1,11 @@
+FENG = 80 #foundation engliash score
+GB = 80 #general business score
+ICS = 85 #introduction to computer system score
+CP = 90 #computer programing score 
+
+print ("--- Your Score---")
+print ("Foundation English:",FENG)
+print ("General Busineess:",GB)
+print ("Introduction to Computer System:",ICS)
+print ("Computer Programming:",CP)
+
