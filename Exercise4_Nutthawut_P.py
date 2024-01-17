b25ed1512066df1b43b43bd98f235c1d25f184af
NutthawutS <nutthawut.saibua@gmail.com>
2024-01-17 18:22:55 +0700

    Create Exercise4_Nutthawut_P.py

---
+++ Exercise4_Nutthawut_P.py
@@ -0,0 +1,16 @@
+"""
+Subject dictionary
+FE = Foundation English
+GB = General Business
+ICS = Introduction to Computer Systems
+CP Computer Programming
+"""
+FE = 80.0
+GB = 80.0
+ICS = 80.0
+CP = 80.0
+print("--- Your Score ---")
+print("Foundation English :", FE)
+print("General Business :", GB)
+print("Introduction to Computer Systems :", ICS)
+print("Computer Programming :", CP)
